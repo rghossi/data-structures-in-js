f313e1978995612232ca940de2537a9a3fa24549
rghossi <rafael.ghossi@gmail.com>
2017-02-17 09:32:44 -0200

    passing tests for peek, pop and push

--- src/Stack.js
+++ src/Stack.js
@@ -1,3 +1,18 @@
 export default class Stack {
+	constructor() {
+		this.stack = [];
+	}
+
+	peek() {
+		return this.stack[this.stack.length-1] || -1;
+	}
+
+	pop() {
+		return -1;
+	}
+
+	push() {
+		this.stack.push(1);
+	}
 
 }
